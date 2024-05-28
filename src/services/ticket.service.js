16aa71789f9ef2c88eb63375b5ad28facdcee121
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-05-28 14:39:29 +0700

    new api

--- src/services/ticket.service.js
+++ src/services/ticket.service.js
@@ -161,6 +161,15 @@ const getTicketById = async (id) => {
             }
         }
 
+        const carList = await db.Xe.findAll()
+
+        for (let i = 0; i < carList.length; i++) {
+            if (carList[i].id == ticket[0].id_xe) {
+                ticket[0].ten_nha_xe = carList[i].ten_nha_xe
+                ticket[0].so_cho = carList[i].so_cho
+            }
+        }
+
         return ticket
     }
     catch (e) {
