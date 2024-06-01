8a5d560f4746c35f0f545c47b2cc6240c66e2569
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-06-01 21:27:02 +0700

    purchase api

--- src/services/billing.service.js
+++ src/services/billing.service.js
@@ -2,9 +2,15 @@
 const db = require('../models/index');
 const solveBilling = async (bill, qrcode, userId) => {
     try {
+
+        const user = await db.NguoiDung.findOne({
+            where: {
+                ten: userId
+            }
+        })
         await db.Ve.create({
             id_chuyen_di: bill.id_chuyen_di,
-            id_nguoi_dung: userId,
+            id_nguoi_dung: user.id,
             qr_code: qrcode,
             created_at: bill.time,
             used: 0,
